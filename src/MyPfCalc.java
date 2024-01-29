224410ffec45bc5ca661f6b0494bd86504bc67a6
JusSolo <Sol201598@uvg.edu.gt>
2024-01-29 16:34:59 -0600

    Se creo la calculadora

--- src/MyPfCalc.java
+++ src/MyPfCalc.java
@@ -1,13 +1,52 @@
+/**
+ *
+ */
 public class MyPfCalc implements PosfixCalc{
-    private UvgStack pila;
+    private UvgStack numeros;
 
 
-    public MyPfCalc(){
-
+    public MyPfCalc(UvgStack pila){
+        this.numeros = pila;
     }
 
     @Override
+
     public Integer calc(String problem) {
-        return null;
+        for (int i = 0; i < problem.length(); i++){
+            UvgStack numeros = pila;
+            char v = (char) problem.charAt(i);
+
+            if ( Character.isDigit(v) ){
+                numeros.push(Integer.parseInt(String.valueOf(v)));
+            }
+            switch (v){
+                case '+':
+                    respuesta = numeros.pull() + numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case '-':
+                    respuesta = numeros.pull() - numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case '*':
+                    respuesta = numeros.pull() * numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case '/':
+                    respuesta = numeros.pull() / numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                default:
+
+
+
+            }
+
+        }
+        return numeros.pull();
     }
 }
